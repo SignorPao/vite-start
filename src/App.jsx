f221ad1fe5f88a0f895d5de614fe142ede0e03b4
Pavel Belousov <signorpao@gmail.com>
2023-03-12 11:53:20 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-[6vmax] text-red-600">Vite is here!</h1>
+    </div>
+  );
+}
+
+export default App;
